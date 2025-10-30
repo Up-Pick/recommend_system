a4cebff0a99d69e4411f35c350eb6c7ecebdc4b6
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-10-30 20:00:10 +0900

    init project

---
+++ src/main/java/uppick/uppickrecommend/UpPickRecommendApplication.java
@@ -0,0 +1,14 @@
+package uppick.uppickrecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpPickRecommendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UpPickRecommendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uppick/uppickrecommend/UpPickRecommendApplicationTests.java
@@ -0,0 +1,14 @@
+package uppick.uppickrecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpPickRecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
